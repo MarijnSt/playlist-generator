1b091e69e32ce98a9251356c8b72e836b8771588
Marijn Stammeleer <marijn.stammeleer@gmail.com>
2024-04-27 14:15:16 +0200

    feat: handle spotify login

---
+++ app/Actions/Auth/LoginWithSpotify.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Actions\Auth;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class LoginWithSpotify
+{
+    use AsAction;
+
+    public function asController(): RedirectResponse
+    {
+        $spotifyUser = Socialite::driver('spotify')->user();
+
+        // save user in db
+        $user = User::updateOrCreate(
+            ['email' => $spotifyUser->email],
+            [
+                'name' => $spotifyUser->name,
+                'email' => $spotifyUser->email,
+                'spotify_id' => $spotifyUser->id,
+            ]
+        );
+
+        // save tokens in session
+        session([
+            'spotify_token' => $spotifyUser->token,
+            'spotify_refresh_token' => $spotifyUser->refreshToken,
+            'spotify_expires_in' => now()->addSeconds($spotifyUser->expiresIn),
+        ]);
+
+        // authenticate user
+        Auth::login($user, remember: true);
+
+        // redirect to dashboard
+        return redirect()->route('dashboard');
+    }
+}
